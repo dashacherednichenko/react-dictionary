d9719be5a40c0a3083bd01a3ebcc1cacc6fdd2b8
dashacherednichenko <dasha.cherednichenko@gmail.com>
2022-10-03 17:23:37 +0300

    add bootstrap, create first component

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="Footer">
+            <small>
+                <a
+                    href="https://github.com/dashacherednichenko/react-dictionary"
+                    target="_blank"
+                >
+                    Open-source code
+                </a>
+                , by Dariia Piven
+            </small>
+        </footer>
+    );
+}
