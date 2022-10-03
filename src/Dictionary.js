cbb1e2739f2c114ff19ee7beae4a82f1851a013c
dashacherednichenko <dasha.cherednichenko@gmail.com>
2022-10-03 18:18:03 +0300

    make search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Dictionary.css"
 
-export default function Dictionary(){
-    return <div className="Dictionary">test</div>;
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState('');
+    function search(e){
+        e.preventDefault();
+        console.log("search", keyword);
+        alert(`User search for "${keyword}"`);
+    }
+
+    function handleSearchInputChange (e){
+        console.log("search.input", e.target.value);
+        setKeyword(e.target.value);
+    }
+
+    return (<div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" onChange={handleSearchInputChange}/>
+        </form>
+    </div>);
 }
