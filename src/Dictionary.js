51e6f40f3a740af81f9f6cdcaaa48e46eadcbad4
dashacherednichenko <dasha.cherednichenko@gmail.com>
2022-10-05 18:12:25 +0300

    display definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,34 +1,43 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Result from "./Result";
 
 import "./Dictionary.css"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState('');
+    let [results, setResults] = useState({});
 
-    function handleResponse(res){
+    function handleResponse(res) {
         let data = res.data;
-        for (let i = 0; i < data.length; i++) {
-            console.log('res', data[i]);
-        }
+        setResults( res.data );
+        // for (let i = 0; i < data.length; i++) {
+        //     let defs = data[i].meanings[0].definitions;
+        //     for (let i = 0; i < defs.length; i++) {
+        //         console.log('definition', defs[i].definition);
+        //     }
+        // }
     }
 
-    function search(e){
+    function search(e) {
         e.preventDefault();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        console.log("search", keyword);
+        // console.log("search", keyword);
         axios(apiUrl).then(handleResponse);
         // alert(`User search for "${keyword}"`);
     }
 
-    function handleSearchInputChange (e){
-        console.log("search.input", e.target.value);
+    function handleSearchInputChange(e) {
+        // console.log("search.input", e.target.value);
         setKeyword(e.target.value);
     }
 
-    return (<div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search" onChange={handleSearchInputChange}/>
-        </form>
-    </div>);
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleSearchInputChange}/>
+            </form>
+            <Result results={results} />
+        </div>
+    );
 }
